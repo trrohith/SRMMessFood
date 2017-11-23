8ea4bf5cecc92f3da370e881d06a0c9a8c2135cc
T.R.Rohith <trrohith1@gmail.com>
2017-11-23 22:25:46 +0530

    Messenger

--- app.js
+++ app.js
@@ -291,42 +291,22 @@ function handleApiAiResponse(sender, response) {
 	console.log(JSON.stringify(response));
 	let responseText = response.result.fulfillment.speech;
 	let responseData = response.result.fulfillment.data;
-	let messages = response.result.fulfillment.data.google.richResponse.suggestions[0];
+	let messages = response.result.fulfillment.data.google.richResponse.suggestions;
 	let action = response.result.action;
 	let contexts = response.result.contexts;
 	let parameters = response.result.parameters;
 	console.log(JSON.stringify(messages));
 	sendTypingOff(sender);
 
-	if (isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1)) {
-		let timeoutInterval = 1100;
-		let previousType;
-		let cardTypes = [];
-		let timeout = 0;
-		for (var i = 0; i < messages.length; i++) {
-
-			if (previousType == 1 && (messages[i].type != 1 || i == messages.length - 1)) {
-
-				timeout = (i - 1) * timeoutInterval;
-				setTimeout(handleCardMessages.bind(null, cardTypes, sender), timeout);
-				cardTypes = [];
-				timeout = i * timeoutInterval;
-				setTimeout(handleMessage.bind(null, messages[i], sender), timeout);
-			} else if (messages[i].type == 1 && i == messages.length - 1) {
-				cardTypes.push(messages[i]);
-				timeout = (i - 1) * timeoutInterval;
-				setTimeout(handleCardMessages.bind(null, cardTypes, sender), timeout);
-				cardTypes = [];
-			} else if (messages[i].type == 1) {
-				cardTypes.push(messages[i]);
-			} else {
-				timeout = i * timeoutInterval;
-				setTimeout(handleMessage.bind(null, messages[i], sender), timeout);
-			}
-
-			previousType = messages[i].type;
-
-		}
+	if (isDefined(messages) && (messages.length > 1)) {
+		let message = {};
+		message.push({"type":"2"});
+		message.push({"title":responseText});
+		messages.forEach(element => {
+			message.push({"replies":element.title});
+		});
+		console.log(message);
+		handleMessage(message, sender);
 	} else if (responseText == '' && !isDefined(action)) {
 		//api ai could not evaluate input.
 		console.log('Unknown query' + response.result.resolvedQuery);
