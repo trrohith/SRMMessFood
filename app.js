d9a1e6c4e0af5b36364caeee595fd13f39dcd22d
T.R.Rohith <trrohith1@gmail.com>
2017-11-23 17:02:39 +0530

    Test

--- app.js
+++ app.js
@@ -79,15 +79,47 @@ app.post('/googlewebhook', function (req, res) {
 	  res.setHeader('Content-Type', 'application/json');
 	
 	  var response = `You have gotten the backend code to talk!`;
-	  res.send(JSON.stringify({ "speech": response, "displayText": response,
-	  "replies": [
-		"Quick reply 1",
-		"Quick reply 2",
-		"Quick reply 3"
-	  ],
-	  "title": "Quick Reply Title",
-	  "type": 2
-  }));
+	  res.send(JSON.stringify({  "conversationToken": "",
+	  "expectUserResponse": true,
+	  "expectedInputs": [
+		  {
+			  "inputPrompt": {
+				  "richInitialPrompt": {
+					  "items": [
+						  {
+							  "simpleResponse": {
+								  "textToSpeech": "Math and prime numbers it is!"
+							  }
+						  },
+						  {
+							  "basicCard": {
+								  "title": "Math & prime numbers",
+								  "formattedText": "42 is an even composite number. It\n    is composed of three distinct prime numbers multiplied together. It\n    has a total of eight divisors. 42 is an abundant number, because the\n    sum of its proper divisors 54 is greater than itself. To count from\n    1 to 42 would take you about twenty-one…",
+								  "image": {
+									  "url": "https://example.google.com/42.png",
+									  "accessibilityText": "Image alternate text"
+								  },
+								  "buttons": [
+									  {
+										  "title": "Read more",
+										  "openUrlAction": {
+											  "url": "https://example.google.com/mathandprimes"
+										  }
+									  }
+								  ]
+							  }
+						  }
+					  ],
+					  "suggestions": []
+				  }
+			  },
+			  "possibleIntents": [
+				  {
+					  "intent": "actions.intent.TEXT"
+				  }
+			  ]
+		  }
+	  ]}));
 });
 
 	
