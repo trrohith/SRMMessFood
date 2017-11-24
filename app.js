b7f3eae9178faa892043668e712a900c1fce1397
T.R.Rohith <trrohith1@gmail.com>
2017-11-24 06:06:17 +0530

    typo

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.post('/googlewebhook/', function (req, res) {
 	}
 	var date = new Date(DateWanted.substring(0, 10));
 	date = addMinutes(date, 330);
-	if(JSON.stringify(action)==='SET_MESS'||JSON.stringify==='\"SET_MESS\"'){
+	if(JSON.stringify(action)==='SET_MESS'||JSON.stringify(action)==='\"SET_MESS\"'){
 		if(!validMess(messName)){ 
 			askToSetMess(function(toSendValue){
 				res.send(toSendValue);
