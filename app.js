dd7660a9c0d8095f800118aeddd366ca559fc91b
T.R.Rohith <trrohith1@gmail.com>
2017-12-16 23:04:34 +0530

    url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const uuid = require('uuid');
 var messNamesArray = [];
 var mysql = require('mysql');
 var con = mysql.createConnection({
-	host: "webarchsrm.com",
+	host: "localhost",
 	user: "srmmess",
 	password: "srmmess#2018",
 	database: "srmmess"
